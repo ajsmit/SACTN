de0f75cd037b13f929d60d4af58da9da41689791
schrob040 <wiederweiter@gmail.com>
2016-01-28 16:59:04 +0200

    The first concrete step towards establishing v4.0. The base daily data product is loaded as well as the site list.

---
+++ munge/01-site.list.R
@@ -0,0 +1,17 @@
+#############################################################################
+###"munge/01-site.list.R"
+## This script does:
+# 1. Reads in site_list_v4.0.csv;
+# 2. Runs script that ensures correct ordering of sites
+## DEPENDS ON:
+# Nothing
+## USED BY:
+# (expand on this)
+## CREATES:
+# Nothing
+#############################################################################
+
+#############################################################################
+## Read in the .csv file
+  # This file is compiled manually
+site.list <- read.csv("metadata/site_list_v4.0.csv")
