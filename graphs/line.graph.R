77d587964fce3704f19d9a045da658c71df88dc7
schrob040 <wiederweiter@gmail.com>
2016-03-14 20:35:32 +0200

    Updated code to uptake any new data available from SACTNraw files. Also added a script that allows one to create line graphs. Still playing around with the capabilities of the 'ProjectTemplate' package.

---
+++ graphs/line.graph.R
@@ -0,0 +1,31 @@
+#############################################################################
+###"graph/line.graph.R"
+## This script does:
+# 1. Subsets monthly data as preferred;
+# 2. Creates line graph of data
+## DEPENDS ON:
+require(ggplot2)
+## USED BY:
+# Nothing
+## CREATES:
+# Results may vary
+#############################################################################
+
+#############################################################################
+## Subsets monthly data as preferred
+monthlyData <- droplevels(SACTNmonthly_v4.0[SACTNmonthly_v4.0$src == "DAFF",])
+monthlyData$date <- as.Date(monthlyData$date) # convert from "POSIXct" to "Date" for plotting
+
+#############################################################################
+## Creates line graph of data
+
+# Here is the base code from which one can play around to create new things
+lg <- ggplot(data = monthlyData, aes(x = date, y = temp)) + bw_update +
+  #geom_line() +
+  geom_line(aes(colour = site)) +
+  facet_grid(site ~ .) +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y", expand = c(0.015,0)) +
+  ylab(expression(paste("Temperature (", degree~C, ")"))) + xlab("Date") +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
+lg
+ggsave("graphs/DAFFsites.pdf", height = 12, width = 12)
---
+++ munge/02-data.R
@@ -0,0 +1,36 @@
+#############################################################################
+###"munge/02-daily.data.R"
+## This script does:
+# 1. Read in the raw data if there is access to it
+## DEPENDS ON:
+# Nothing
+## USED BY:
+# Nothing
+## CREATES:
+# Nothing
+#############################################################################
+
+#############################################################################
+## Read in the raw data if there is access to it
+# Otherwise only uses the data that were downloaded from GitHub
+
+# Daily data
+if(file.exists("~/SACTNraw/data/SACTNdaily_v4.0.Rdata")){
+  load("~/SACTNraw/data/SACTNdaily_v4.0.Rdata")
+  save(SACTNdaily_v4.0, file = "data/SACTNdaily_v4.0.Rdata")
+}
+# Monthly data
+if(file.exists("~/SACTNraw/data/SACTNmonthly_v4.0.Rdata")){
+  load("~/SACTNraw/data/SACTNmonthly_v4.0.Rdata")
+  save(SACTNmonthly_v4.0, file = "data/SACTNmonthly_v4.0.RData")
+}
+# Annual data
+if(file.exists("~/SACTNraw/data/SACTNannual_v4.0.Rdata")){
+  load("~/SACTNraw/data/SACTNannual_v4.0.Rdata")
+  save(SACTNannual_v4.0, file = "data/SACTNannual_v4.0.Rdata")
+}
+# Climatology data
+if(file.exists("~/SACTNraw/data/SACTNclim_v4.0.Rdata")){
+  load("~/SACTNraw/data/SACTNclim_v4.0.Rdata")
+  save(SACTNclim_v4.0, file = "data/SACTNclim_v4.0.Rdata")
+}
