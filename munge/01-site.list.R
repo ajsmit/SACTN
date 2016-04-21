ec29e50583f80f79d91cf5bd63f20e02a0c53ce3
schrob040 <wiederweiter@gmail.com>
2016-04-22 00:24:28 +0200

    Update to minor SAEON monthly data issue. NA values now added correctly.

--- munge/01-site.list.R
+++ munge/01-site.list.R
@@ -14,4 +14,8 @@
 #############################################################################
 ## Read in the .csv file
   # This file is compiled manually
+if(file.exists("~/SACTNraw/metadata/site_list_v4.0.csv")){
+  site_list <- read.csv("~/SACTNraw/metadata/site_list_v4.0.csv")
+  write.csv(site_list, file = "metadata/site_list_v4.0.csv", row.names = F)
+}
 site_list <- read.csv("metadata/site_list_v4.0.csv")
