b55adff54f623e86d580f7d9cd3ac2636886b347
AJ Smit <albertus.smit@gmail.com>
2016-06-12 08:27:29 +0200

    Produce the REAMME.Rmd file, with updates.

---
+++ leaflet.R
@@ -0,0 +1,16 @@
+library(leaflet)
+sites <- read.csv("metadata/site_list_v4.0.csv")
+
+pal <- colorFactor(palette = c("yellow", "orange", "green", "red", "turquoise", "navy", "blue"), 
+                   domain = c("DAFF", "DEA", "EKZNW", "KZNSB", "SAEON", "SAWS", "UWC"))
+
+content <- paste(sep = "<br/>", "<b>", sites$site, "</b>", sites$src, sites$type)
+
+m1 = leaflet() %>%
+  addProviderTiles("Esri.OceanBasemap",
+                   options = providerTileOptions(opacity = 1.0)) %>%
+  addCircleMarkers(lng = sites$lon, lat = sites$lat, group = sites$src,
+                   popup = content, radius = 5, color = pal(sites$src),
+                   stroke = FALSE, fillOpacity = 0.8) %>%
+  setView(24, -31, zoom = 6)
+m1
