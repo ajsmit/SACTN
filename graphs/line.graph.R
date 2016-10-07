3e94b801b306efe49010a973c755a9afe83d7c7e
schrob040 <wiederweiter@gmail.com>
2016-10-07 11:16:19 +0200

    New UWC sites and data added. Improved line.graph.R

--- graphs/line.graph.R
+++ graphs/line.graph.R
@@ -13,8 +13,14 @@ require(ggplot2)
 
 #############################################################################
 ## Subsets monthly data as preferred
-monthlyData <- droplevels(SACTNmonthly_v4.1[SACTNmonthly_v4.1$src == "EKZNW",])
+monthlyData <- droplevels(SACTNmonthly_v4.1[SACTNmonthly_v4.1$src == "UWC",])
 monthlyData$date <- as.Date(monthlyData$date) # convert from "POSIXct" to "Date" for plotting
+
+## Create labeler to show site names in the correct order
+site.names <- function(sites) {
+  sites <- list(sapply(strsplit(as.character(levels(monthlyData$index)), "/"), "[[", 1))
+  return(sites)
+}
 
 #############################################################################
 ## Creates line graph of data
@@ -23,10 +29,10 @@ monthlyData$date <- as.Date(monthlyData$date) # convert from "POSIXct" to "Date"
 lg <- ggplot(data = monthlyData, aes(x = date, y = temp)) + bw_update +
   #geom_line() +
   geom_line(aes(colour = site)) +
-  facet_grid(site ~ .) +
+  facet_grid(index ~ ., labeller = site.names) +
   scale_x_date(date_breaks = "1 year", date_labels = "%Y", expand = c(0.015,0)) +
   ylab(expression(paste("Temperature (", degree~C, ")"))) + xlab("Date") +
   theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
         legend.position = "none")
 lg
-ggsave("graphs/EKZNWsites.pdf", height = 12, width = 12, limitsize = FALSE)
+ggsave("graphs/UWCsites.pdf", height = 12, width = 12, limitsize = FALSE)
