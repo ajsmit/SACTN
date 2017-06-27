4048db644a3f425a3ea201f80eb173d89f6d7916
schrob040 <wiederweiter@gmail.com>
2017-06-27 12:22:40 +0200

    Time series extracting script

---
+++ data/extract.ts.R
@@ -0,0 +1,24 @@
+#############################################################################
+###"data/extract.ts.R"
+## This script does:
+# 1. Extracts one or many specific time series from the monthly data
+## DEPENDS ON:
+library(tidyverse)
+## USED BY:
+# Whenever anyone makes a data request
+## CREATES:
+#
+#############################################################################
+
+load("data/SACTNmonthly_v4.1.Rdata")
+print(unique(SACTNmonthly_v4.1$index))
+
+extract.ts <- function(ts_index, file_name){
+  SACTNmonthly_extract <- filter(SACTNmonthly_v4.1, index %in% ts_index)
+  if(length(SACTNmonthly_extract) == 0){
+    warning("Make sure the correct site index(es) is being provided. 
+            The index(es) must be given as a character vector, or series thereof.")
+  }
+  save(SACTNmonthly_extract, file = paste0("~/Desktop/",file_name,".Rdata"))
+  write_csv(SACTNmonthly_extract, path = paste0("~/Desktop/",file_name,".csv"))
+}
