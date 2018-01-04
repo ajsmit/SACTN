19d4412e45abb93a982bdf43b2ebde8a4e16963b
schrob040 <wiederweiter@gmail.com>
2018-01-04 15:17:53 +0200

    SAWS 2017 data uploaded

--- data/interpolate.R
+++ data/interpolate.R
@@ -17,34 +17,39 @@ library(akima)
 # 1. Load function required to interpolate data ---------------------------
 
 # This function takes a single user defined set of coordinates (coords): lon/ lat
-# The user may also  input the desired site name (name)
+# It also takes into account the given/desired sie name
 # As well as a user defined dataframe (df) of temperature time series
-# It then screens out time series baserd on a user defined range of dates (start, end): "YYYY-MM-DD"
 # It then creates a time series for that point based on the nearest n (n) time series
-# If the nearest time series are not long enough the next nearest time series are used instead
-# A warning is returned when this is done
-# The user may define if they want the function to expand (TRUE) the range used for interpolation or not (FALSE)
 
 ## For testing ##
 # coords <- c(28.431673, -32.638033)
 # coords <- c(17.89, -31.33)
-# name <- "Qolora"
 # df <- SACTNmonthly_v4.1
 # n <- 5
-# start <- "2005-01-01"
-# end <- "2014-12-31"
-# expand <- TRUE
 ##
+
+load("data/SACTNmonthly_v4.1.Rdata")
+load("metadata/site_list_v4.1.Rdata")
+
+interpolate <- function(coords, df = SACTNmonthly_v4.1, sites = site_list, n = 5){
+  # if(is.data.frame(coords) == F){
+  #   coords <- data.frame(lon = coords[1], lat = coords[2])
+  # }
+  # sites1 <- site_list
+  # sites1 <- site_list[site_list$date.start <= as.Date(start),]
+  # sites1 <- sites1[sites1$date.end >= as.Date(end),]
+  # if(length(sites1$site) < 1){
+  #   stop("No time series cover the given range of dates. Please redefine the given date range.")
+  # }
   
-interpolate <- function(coords, name = "New Site", df = SACTNmonthly_v4.1, n = 5, start = "2005-01-01", end = "2014-12-31", expand = TRUE){
-  if(is.data.frame(coords) == F){
-    coords <- data.frame(lon = coords[1], lat = coords[2])
-  }
-  sites1 <- site_list[site_list$date.start <= as.Date(start),]
-  sites1 <- sites1[sites1$date.end >= as.Date(end),]
-  if(length(sites1$site) < 1){
-    stop("No time series cover the given range of dates. Please redefine the given date range.")
-  }
+  
+  
+  
+  # Expand coastal coordinates for more accurate interpolating
+  lon2 <- akima::aspline(coastline$x, n = length(coastline$x)*10)$y
+  lat2 <- akima::aspline(coastline$y, n = length(coastline$y)*10)$y
+  
+  
   nn <- n-1
   df2 <- data.frame()
   while(length(df2) <= 12){
@@ -60,14 +65,14 @@ interpolate <- function(coords, name = "New Site", df = SACTNmonthly_v4.1, n = 5
   }
   site <- data.frame(site = name, adply(df3[2:length(df3)], 2, intrp))[,1:3]
   site$X1 <- as.Date(site$X1)
-  if(length(site$V1[complete.cases(site$V1)]) == 0){
-    sites3 <- droplevels(sites1[as.integer(knnx.index(sites1[,5:6], coords, k = 2)),])
-    df4 <- df[df$index %in% sites3$index,]
-    df5 <- dcast(df4, index ~ date, mean, value.var="temp")
-    df6 <- df5[, colSums(is.na(df5)) == 0]
-    site <- data.frame(site = name, adply(df6[2:length(df6)], 2, colMeans))
-    site$X1 <- as.Date(site$X1)
-  }
+  # if(length(site$V1[complete.cases(site$V1)]) == 0){
+  #   sites3 <- droplevels(sites1[as.integer(knnx.index(sites1[,5:6], coords, k = 2)),])
+  #   df4 <- df[df$index %in% sites3$index,]
+  #   df5 <- dcast(df4, index ~ date, mean, value.var="temp")
+  #   df6 <- df5[, colSums(is.na(df5)) == 0]
+  #   site <- data.frame(site = name, adply(df6[2:length(df6)], 2, colMeans))
+  #   site$X1 <- as.Date(site$X1)
+  # }
   colnames(site)[2:3] <- c("date", "temp")
   return(site)
 }
